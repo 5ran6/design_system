05378249640a88446fd3ac854cc26ae7b005e212
Francis Ocholi <5raan6@gmail.com>
2022-10-03 02:02:33 +0100

    Update app.dart
    reverted back navigator

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,4 @@
 import 'package:canton_design_system/canton_design_system.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CantonApp extends StatelessWidget {
   final String? title;
@@ -8,6 +7,7 @@ class CantonApp extends StatelessWidget {
   final Color? primaryDarkColor;
   final Color primaryLightVariantColor;
   final Color primaryDarkVariantColor;
+  final List<NavigatorObserver>? navigatorObservers;
   final Widget Function(BuildContext, Widget?)? builder;
   final ThemeData? lightTheme, darkTheme;
 
@@ -18,6 +18,7 @@ class CantonApp extends StatelessWidget {
     required this.primaryDarkColor,
     this.primaryLightVariantColor = CantonColors.blue,
     this.primaryDarkVariantColor = CantonDarkColors.blue,
+    this.navigatorObservers,
     this.builder,
     this.lightTheme,
     this.darkTheme,
@@ -25,27 +26,24 @@ class CantonApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, watch, child) {
-        final repo = watch(themeProvider);
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: title!,
-          themeMode: repo.themeMode,
-          theme: lightTheme ??
-              cantonLightTheme().copyWith(
-                primaryColor: primaryLightColor,
-                colorScheme: cantonLightTheme().colorScheme.copyWith(primaryVariant: primaryLightVariantColor),
-              ),
-          darkTheme: darkTheme ??
-              cantonDarkTheme().copyWith(
-                primaryColor: primaryDarkColor,
-                colorScheme: cantonDarkTheme().colorScheme.copyWith(primaryVariant: primaryDarkVariantColor),
-              ),
-          home: home,
-          builder: builder,
-        );
-      },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title!,
+      navigatorObservers: navigatorObservers ?? [],
+      theme: lightTheme ??
+          cantonLightTheme().copyWith(
+            primaryColor: primaryLightColor,
+            colorScheme: cantonLightTheme().colorScheme.copyWith(
+                primaryVariant: primaryLightVariantColor),
+          ),
+      darkTheme: darkTheme ??
+          cantonDarkTheme().copyWith(
+            primaryColor: primaryDarkColor,
+            colorScheme: cantonDarkTheme().colorScheme.copyWith(
+                primaryVariant: primaryDarkVariantColor),
+          ),
+      home: home,
+      builder: builder,
     );
   }
 }
