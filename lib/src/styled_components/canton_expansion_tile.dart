8e3ab63ad5ed24be3d63be335e50c5be95ceb9dc
Igbana Israel <igbanaaondosoo13@gmail.com>
2024-10-16 10:39:47 +0100

    fix: Updated all outdated keywords in theme styles to be used in Elisha app

--- lib/src/styled_components/canton_expansion_tile.dart
+++ lib/src/styled_components/canton_expansion_tile.dart
@@ -114,7 +114,7 @@ class _CantonExpansionTileState extends State<CantonExpansionTile> with SingleTi
     _iconColor = _controller.drive(_iconColorTween.chain(_easeInTween));
     _backgroundColor = _controller.drive(_backgroundColorTween.chain(_easeOutTween));
 
-    _isExpanded = PageStorage.of(context)?.readState(context) ?? widget.initiallyExpanded;
+    _isExpanded = PageStorage.of(context).readState(context) ?? widget.initiallyExpanded;
     if (_isExpanded) _controller.value = 1.0;
   }
 
@@ -137,7 +137,7 @@ class _CantonExpansionTileState extends State<CantonExpansionTile> with SingleTi
           });
         });
       }
-      PageStorage.of(context)?.writeState(context, _isExpanded);
+      PageStorage.of(context).writeState(context, _isExpanded);
     });
     if (widget.onExpansionChanged != null) widget.onExpansionChanged!(_isExpanded);
   }
